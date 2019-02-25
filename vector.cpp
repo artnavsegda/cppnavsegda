921693190a5d209b5cbe96b340c30015bff54a82
Art Navsegda <artnavsegda@gmail.com>
2019-02-25 05:01:55 +0300

    vector

--- vector.cpp
+++ vector.cpp
@@ -3,7 +3,11 @@
 int main()
 {
 	vector<int> v(6,42);
-	cout << "Vector size is " << v.size() << endl;
-	cout << "Vector element 3 is " << v[3] << endl;
+	cout << "Vector v size is " << v.size() << endl;
+	cout << "Vector v element 3 is " << v[3] << endl;
+	vector<int> r;
+	r.push_back(44);
+	cout << "Vector r size is " << r.size() << endl;
+	cout << "Vector v element 0 is " << v[0] << endl;
 	return 0;
 }
