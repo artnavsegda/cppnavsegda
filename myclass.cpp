8d9be64152fa329675d2aaad98ff93d63b315b07
Art Navsegda <artnavsegda@gmail.com>
2019-06-14 18:51:55 +0300

    Create myclass.cpp

---
+++ myclass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class myclass
+{
+	public:
+		int i;
+		void printi()
+		{
+			cout << i << endl;
+		}
+};
+
+int main()
+{
+	myclass myobj;
+	myobj.i = 10;
+	myobj.printi();
+}
+
