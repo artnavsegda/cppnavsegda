b2d812af0db6ddc37565dbc5cf5266bc158efbc8
Art Navsegda <artnavsegda@gmail.com>
2019-06-15 05:36:11 +0300

    Create overload.cpp

---
+++ language/overload.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct myclass
+{
+	myclass();
+	myclass(myclass &obj);
+	myclass(myclass &&obj);
+	~myclass();
+	int i;
+	void printi();
+	void operator !();
+};
+
+struct otherclass : myclass
+{
+	int x;
+};
+
+myclass::myclass()
+{
+	cout << "Class created\n";
+}
+
+myclass::myclass(myclass &obj)
+{
+	cout << "Class copied\n";
+}
+
+myclass::myclass(myclass &&obj)
+{
+	cout << "Class moved\n";
+}
+
+myclass::~myclass()
+{
+	cout << "Class destroyed\n";
+}
+
+void myclass::operator !()
+{
+	cout << "Double cross\n";
+}
+
+void myclass::printi()
+{
+	cout << i << endl;
+}
+
+int main()
+{
+	myclass myobj;
+	myobj.i = 10;
+	myobj.printi();
+	!myobj;
+	myclass myobj2 = myobj;
+	otherclass myobj3;
+	myobj3.i = 11;
+	myobj3.printi();
+}
+
