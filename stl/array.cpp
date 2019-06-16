af2c7003cbe143dc45fbb9ab8e5f4ae46f47fff1
Art Navsegda <artnavsegda@gmail.com>
2019-06-16 22:22:37 +0300

    Create array.cpp

---
+++ stl/array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	array<int, 3> myarray = { 1, 2, 3 };
+	cout << myarray[0];
+	cout << myarray[1];
+	cout << myarray[2];
+	cout << "\n";
+}
+
