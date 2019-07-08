202339c92d38592318f99420c115a4dfe4bd0cd8
Art Navsegda <artnavsegda@gmail.com>
2019-07-08 14:53:58 +0300

    Create private.cpp

---
+++ language/class/private.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class myclass
+{
+	int i;
+	public:
+		void printi();
+		void seti(int itoset);
+};
+
+void myclass::printi()
+{
+	cout << i << endl;
+}
+
+void myclass::seti(int itoset)
+{
+	i = itoset;
+}
+
+int main()
+{
+	myclass myobj;
+	myobj.seti(20);
+	myobj.printi();
+}
+
