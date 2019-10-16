723347c25425efce5d2e20afa7af9697d8e1cf43
Art Navsegda <artnavsegda@gmail.com>
2019-10-16 11:24:38 +0300

    virtual

--- language/class/virtual.cpp
+++ language/class/virtual.cpp
@@ -1,26 +1,36 @@
-#include <iostream>
-using namespace std;
-
-struct myclass
-{
-	virtual void printi() = 0;
-};
-
-struct otherclass : myclass
-{
-	int i;
-	void printi();
-};
-
-void otherclass::printi()
-{
-	cout << i << endl;
-}
-
-int main()
-{
-	otherclass myobj;
-	myobj.i = 11;
-	myobj.printi();
-}
+#include<iostream> 
+using namespace std; 
+  
+class base 
+{ 
+public: 
+    virtual void print () 
+    { cout<< "print base class" <<endl; } 
+  
+    void show () 
+    { cout<< "show base class" <<endl; } 
+}; 
+  
+class derived:public base 
+{ 
+public: 
+    void print () 
+    { cout<< "print derived class" <<endl; } 
+  
+    void show () 
+    { cout<< "show derived class" <<endl; } 
+}; 
+  
+int main() 
+{ 
+    base *bptr; 
+    derived d; 
+    bptr = &d; 
+      
+    //virtual function, binded at runtime 
+    bptr->print();  
+      
+    // Non-virtual function, binded at compile time 
+    bptr->show();  
+} 
 
