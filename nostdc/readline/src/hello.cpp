1d3d0e7b89655db7c2eb31bbc45cc4a98ef91ab6
Art Navsegda <artnavsegda@gmail.com>
2019-10-21 15:05:22 +0300

    readline

---
+++ nostdc/readline/src/hello.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main()
+{
+  rl_bind_key('\t', rl_complete);
+
+  while (1)
+  {
+    char * input = readline("prompt> ");
+    if (!input)
+      break;
+    add_history(input);
+    free(input);
+  }
+	return 0;
+}
