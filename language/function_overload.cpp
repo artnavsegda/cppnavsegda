43402df671eec4eabe2cb5cadae254dee1f7aed7
Art Navsegda <artnavsegda@gmail.com>
2019-11-07 09:54:25 +0300

    function overload

--- language/function_overload.cpp
+++ language/function_overload.cpp
@@ -1,1 +1,20 @@
-// to be written
+
+#include <iostream>
+using namespace std;
+
+void print(int i) {
+  cout << " Here is int " << i << endl;
+}
+void print(double  f) {
+  cout << " Here is float " << f << endl;
+}
+void print(char const *c) {
+  cout << " Here is char* " << c << endl;
+}
+
+int main() {
+  print(10);
+  print(10.10);
+  print("ten");
+  return 0;
+}
