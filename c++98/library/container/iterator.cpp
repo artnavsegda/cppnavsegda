e2c853f1fd5723f0432570dc105231d975e33bc2
Art Navsegda <artnavsegda@gmail.com>
2019-11-26 19:14:11 +0300

    Update iterator.cpp

--- c++98/library/container/iterator.cpp
+++ c++98/library/container/iterator.cpp
@@ -1,9 +1,10 @@
-
+#include <iostream>
+#include <vector>
 
 int main()
 {
-	vector<int> v = { 1,2,3,4,5 };
-	vector<int>::iterator ptr;
+	std::vector<int> v = { 1,2,3,4,5 };
+	std::vector<int>::iterator ptr;
 	for (ptr = v.begin(); ptr < v.end(); ptr++)
 		cout << *ptr << endl;
 	return 0;
