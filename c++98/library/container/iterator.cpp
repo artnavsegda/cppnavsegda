af3e2c4e45caa2bbf76db702ef94d04c7f2ddc94
Art Navsegda <artnavsegda@gmail.com>
2019-11-30 22:12:32 +0300

    blya

--- c++98/library/container/iterator.cpp
+++ c++98/library/container/iterator.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 int main()
 {
-	std::vector<int> v = { 1,2,3,4,5 };
-	std::vector<int>::iterator ptr;
+	vector<int> v = { 1,2,3,4,5 };
+	vector<int>::iterator ptr;
 	for (ptr = v.begin(); ptr < v.end(); ptr++)
 		cout << *ptr << endl;
 	return 0;
