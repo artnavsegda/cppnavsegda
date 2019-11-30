1497c6ae6b558d50d7dbcddc4a11e2b0539822e7
Art Navsegda <artnavsegda@gmail.com>
2019-12-01 01:22:06 +0300

    Update set.cpp

--- c++98/library/container/set.cpp
+++ c++98/library/container/set.cpp
@@ -1,76 +1,29 @@
-
-#include <iostream>
-#include <set>
-#include <iterator>
-
-using namespace std;
-
+#include<iostream>
+#include<set>
+#include<string>
 int main()
 {
-    // empty set container
-    set <int, greater <int> > gquiz1;
+	std::set<std::string> setOfNumbers;
 
-    // insert elements in random order
-    gquiz1.insert(40);
-    gquiz1.insert(30);
-    gquiz1.insert(60);
-    gquiz1.insert(20);
-    gquiz1.insert(50);
-    gquiz1.insert(50); // only one 50 will be added to the set
-    gquiz1.insert(10);
+	// Lets insert four elements
+	setOfNumbers.insert("first");
+	setOfNumbers.insert("second");
+	setOfNumbers.insert("third");
+	setOfNumbers.insert("first");
 
-    // printing set gquiz1
-    set <int, greater <int> > :: iterator itr;
-    cout << "\nThe set gquiz1 is : ";
-    for (itr = gquiz1.begin(); itr != gquiz1.end(); ++itr)
-    {
-        cout << '\t' << *itr;
-    }
-    cout << endl;
+	// Search for element in set using find member function
+	std::set<std::string>::iterator it = setOfNumbers.find("second");
+	if(it != setOfNumbers.end())
+		std::cout<<"'first'  found"<<std::endl;
+	else
+		std::cout<<"'first' not found"<<std::endl;
 
-    // assigning the elements from gquiz1 to gquiz2
-    set <int> gquiz2(gquiz1.begin(), gquiz1.end());
+	// Search for element in set using find member function
+	it = setOfNumbers.find("fourth");
+	if(it != setOfNumbers.end())
+		std::cout<<"'fourth'  found"<<std::endl;
+	else
+		std::cout<<"'fourth' not found"<<std::endl;
 
-    // print all elements of the set gquiz2
-    cout << "\nThe set gquiz2 after assign from gquiz1 is : ";
-    for (itr = gquiz2.begin(); itr != gquiz2.end(); ++itr)
-    {
-        cout << '\t' << *itr;
-    }
-    cout << endl;
-
-    // remove all elements up to 30 in gquiz2
-    cout << "\ngquiz2 after removal of elements less than 30 : ";
-    gquiz2.erase(gquiz2.begin(), gquiz2.find(30));
-    for (itr = gquiz2.begin(); itr != gquiz2.end(); ++itr)
-    {
-        cout << '\t' << *itr;
-    }
-
-    // remove element with value 50 in gquiz2
-    int num;
-    num = gquiz2.erase (50);
-    cout << "\ngquiz2.erase(50) : ";
-    cout << num << " removed \t" ;
-    for (itr = gquiz2.begin(); itr != gquiz2.end(); ++itr)
-    {
-        cout << '\t' << *itr;
-    }
-
-    cout << endl;
-
-    //lower bound and upper bound for set gquiz1
-    cout << "gquiz1.lower_bound(40) : "
-         << *gquiz1.lower_bound(40) << endl;
-    cout << "gquiz1.upper_bound(40) : "
-         << *gquiz1.upper_bound(40) << endl;
-
-    //lower bound and upper bound for set gquiz2
-    cout << "gquiz2.lower_bound(40) : "
-         << *gquiz2.lower_bound(40) << endl;
-    cout << "gquiz2.upper_bound(40) : "
-         << *gquiz2.upper_bound(40) << endl;
-
-    return 0;
-
+	return 0;
 }
