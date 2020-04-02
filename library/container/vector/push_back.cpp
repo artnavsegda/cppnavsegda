5e3d576f0c17bc89aa485e1d4c109e98f6025b5a
Art Navsegda <artnavsegda@gmail.com>
2020-04-02 21:21:48 +0300

    Update push_back.cpp

--- library/container/vector/push_back.cpp
+++ library/container/vector/push_back.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 int main()
 {
-	vector<int> v(6,42);
-	cout << "Vector v size is " << v.size() << endl;
-	cout << "Vector v element 3 is " << v[3] << endl;
-	vector<int> r;
+	vector<int> v;
 	r.push_back(44);
-	cout << "Vector r size is " << r.size() << endl;
 	cout << "Vector v element 0 is " << v[0] << endl;
+	cout << "Vector v size is " << v.size() << endl;
 	return 0;
 }
