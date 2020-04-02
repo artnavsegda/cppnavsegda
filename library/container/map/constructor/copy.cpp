c98f51554a909cf06db64bdd9af1ece18bee81a2
Art Navsegda <artnavsegda@gmail.com>
2020-04-02 22:13:17 +0300

    taks

--- library/container/map/constructor/copy.cpp
+++ library/container/map/constructor/copy.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <vector>
+#include <map>
 
 using namespace std;
 
 int main()
 {
-	vector<int> a{10,20,30};
+	map<char,int> m;
+  m['a']=10;
+  m['b']=30;
+  m['c']=50;
+  m['d']=70;
 
-	vector<int> v{a.begin(), a.end()};
-	cout << "Vector v size is " << v.size() << endl;
-	cout << "Vector v element 0 is " << v[0] << endl;
-	cout << "Vector v element 1 is " << v[1] << endl;
-	cout << "Vector v element 2 is " << v[2] << endl;
+	map<char,int> b(m);
+
+	cout << "Map b size is " << b.size() << endl;
+
 	return 0;
 }
--- library/container/map/constructor/range.cpp
+++ library/container/map/constructor/range.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main()
 {
 	map<char,int> m;
-	cout << "Map m size is " << m.size() << endl;
-
   m['a']=10;
   m['b']=30;
   m['c']=50;
   m['d']=70;
 
-	cout << "Map m size is " << m.size() << endl;
+	map<char,int> b(m.begin(), m.end());
+
+	cout << "Map b size is " << b.size() << endl;
 
 	return 0;
 }
