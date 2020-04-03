70bf2818cd8a0e05c511b1c8cc78f19c9fcf241d
Art Navsegda <artnavsegda@gmail.com>
2020-04-03 18:56:05 +0300

    size

--- c++98/library/container/list/size.cpp
+++ c++98/library/container/list/size.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <vector>
+#include <list>
 
 using namespace std;
 
 int main()
 {
-	vector<int> v(6,42);
-	cout << "Vector v size is " << v.size() << endl;
+	list<int> v(6,42);
+	cout << "List v size is " << v.size() << endl;
 	return 0;
 }
