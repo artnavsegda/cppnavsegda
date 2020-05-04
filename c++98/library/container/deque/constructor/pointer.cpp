1d0faea7db3680babd430ba459adb2528f1b96d3
Art Navsegda <artnavsegda@gmail.com>
2020-05-04 07:07:01 +0300

    Update pointer.cpp

--- c++98/library/container/deque/constructor/pointer.cpp
+++ c++98/library/container/deque/constructor/pointer.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <deque>
 
 using namespace std;
 
@@ -7,10 +7,10 @@ int main()
 {
 	int arr[] = { 10, 20, 30 };
 
-	vector<int> v(arr, arr+3);
-	cout << "Vector v size is " << v.size() << endl;
-	cout << "Vector v element 0 is " << v[0] << endl;
-	cout << "Vector v element 1 is " << v[1] << endl;
-	cout << "Vector v element 2 is " << v[2] << endl;
+	deque<int> v(arr, arr+3);
+	cout << "deque v size is " << v.size() << endl;
+	cout << "deque v element 0 is " << v[0] << endl;
+	cout << "deque v element 1 is " << v[1] << endl;
+	cout << "deque v element 2 is " << v[2] << endl;
 	return 0;
 }
