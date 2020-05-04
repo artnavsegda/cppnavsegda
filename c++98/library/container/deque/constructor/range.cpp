4ccec1d0a5c0376c0d556ecf133d15760a58f99c
Art Navsegda <artnavsegda@gmail.com>
2020-05-04 06:57:04 +0300

    1

---
+++ c++98/library/container/deque/constructor/range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> a(3,42);
+
+	vector<int> v(a.begin(), a.end());
+	cout << "Vector v size is " << v.size() << endl;
+	return 0;
+}
