3b767255fe0498f02c1ca60e5c7d6142675f467b
Art Navsegda <artnavsegda@gmail.com>
2020-05-07 14:15:15 +0300

    Update pop.cpp

--- c++98/library/container/stack/pop.cpp
+++ c++98/library/container/stack/pop.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main()
 {
 	stack<int> v;
+	v.push(44);
+	cout << "Stack v size is " << v.size() << endl;
 	v.pop();
 	cout << "Stack v size is " << v.size() << endl;
 	return 0;
