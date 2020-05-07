2e8b35ba8b519c98066db5e3b337091f05f2aafd
Art Navsegda <artnavsegda@gmail.com>
2020-05-07 13:55:56 +0300

    Update size.cpp

--- c++98/library/container/stack/size.cpp
+++ c++98/library/container/stack/size.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
 int main()
 {
-	vector<int> v(6,42);
-	cout << "Vector v size is " << v.size() << endl;
+	stack<int> v;
+	cout << "Stack v size is " << v.size() << endl;
 	return 0;
 }
